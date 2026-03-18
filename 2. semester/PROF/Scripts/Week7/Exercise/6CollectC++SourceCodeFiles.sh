0c98eaa4560b10200b1089d4cb0c77ac68b9e4ef
Asgerfyr <asger.g.stidsen@gmail.com>
2026-03-18 09:48:01 +0100

    Remove unnecessary blank lines and simplify copy command in 6CollectC++SourceCodeFiles.sh

--- 2. semester/PROF/Scripts/Week7/Exercise/6CollectC++SourceCodeFiles.sh
+++ 2. semester/PROF/Scripts/Week7/Exercise/6CollectC++SourceCodeFiles.sh
@@ -6,12 +6,16 @@ IFS=":" #the seperation char for when the array is expanded to a stirng
 DIR_COLLECTED="${$IFS}Collected_Code${$IFS}" # a long string with IFS seperating the directories
 
 
+
 # if we have the collect folder store all the existing directories in the array, otherwise we create the folder
 if [ -d "$COLLECTED_CODE_PATH" ]; then
     DIR_COLLECTED+=$(find "$COLLECTED_CODE_PATH" -type d -printf "%f$IFS")
 else 
     mkdir '/c/Users/Asger/Uni/Github/UNI/2. semester/PROF/Scripts/Week7/Exercise/' Collected_Code
 fi
+
+
+
 
 find . -prune -o -type f \( -name "*.cpp" -o -name "*.h" \) | while read file; do
     dir=$(dirname "$file")
@@ -20,6 +24,6 @@ find . -prune -o -type f \( -name "*.cpp" -o -name "*.h" \) | while read file; d
         continue # if we already have the directory, we skip it
     fi
     DIR_COLLECTED+="$dir$IFS" # add the directory to the array of collected directories
-    cp -r "$dir" "$COLLECTED_CODE_PATH" 2>/dev/null || true # copy the directory to the collected code path, and hide any permission messages
+    cp -r "$dir" "$COLLECTED_CODE_PATH" 2>/dev/null # copy the directory to the collected code path, and hide any permission messages
 done
 
