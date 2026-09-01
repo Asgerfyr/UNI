b24288806389275def8e65a7078f286d5971f145
Asger Grundtdal Stidsen <asger.g.stidsen@gmail.com>
2026-09-01 13:21:13 +0200

    Add basic compilation examples and implement linear search
    - Created a single file compilation example with a simple "Hello World" program.
    - Added a library example demonstrating function usage with `hello.cpp` and `hello.h`.
    - Implemented a class-based example with `Hello` class in `3-main_with_class_v1`.
    - Enhanced class implementation in `3-main_with_class_v2` with method definition in header.
    - Developed an iterator example to demonstrate vector manipulation.
    - Implemented linear search algorithm in `linear_search.h` and created a test file.
    - Added pointer and reference examples to illustrate their usage in C++.
    - Introduced template example with a generic `Pair` class.

---
+++ 3. semester/Algoritmer og datastrukturer/l1/basic_compilation/1-single_file/build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/asger/Documents/GitHub/UNI/3. semester/Algoritmer og datastrukturer/l1/basic_compilation/1-single_file/main.cpp" "CMakeFiles/main.dir/main.cpp.obj" "gcc" "CMakeFiles/main.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
